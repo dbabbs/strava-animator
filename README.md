724f68504486e769c1db79821d2fb7c2bfd2afea
Dylan Babbs <dylan.babbs@here.com>
2019-09-10 10:33:52 -0700

    add instructions

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-# San Juan Walking Map Animation
+# Strava Animator
+
+Make animations based on your Strava data, such as:
+
+- [Seattle run animation](https://twitter.com/dbabbs/status/1171248705686704130/)
+- [Mumbai walk animation](https://twitter.com/dbabbs/status/1142036330957815808)
+- [San Juan walk animation](https://twitter.com/dbabbs/status/1112067431646806016)
 
 ![static](static.png)
 
-An animated map of walking throughout Old San Juan.
 
-View the file `output.mp4` for the final animation. You can also view the live version by cloning this repo and running the project locally.
+## Instructions
+
+1. Download GPX data from Strava
+2. Convert to GeoJSON using [this tool](https://mapbox.github.io/togeojson/)
+3. Replace `data.json` with the converted data
+4. Set the map center in `index.js`
+5. Open up browser and press space bar to start animation
+6. I recommend opening developer tools to set the window size for a good aspect ratio
--- index.js
+++ index.js
@@ -10,54 +10,56 @@ const map = L.map('map', {
    attributionControl: false
 });
 
-const group = L.layerGroup().addTo(map);
+(async () => {
+   const data = await fetch('data.json').then(res => res.json());
 
-const line = data.features[0].geometry.coordinates.map(x => [x[1], x[0]]);
+   const group = L.layerGroup().addTo(map);
+   const line = data.features[0].geometry.coordinates.map(x => [x[1], x[0]]);
 
-const times = data.features[0].properties.coordTimes.map(
-   x => new Date(x)
-      .toLocaleString('en-US', { timeZone: 'America/Los_Angeles' })
-).map(x => {
-   const hours = new Date(x).getHours() - 12;
-   let minutes = new Date(x).getMinutes();
-   if (minutes < 10) {
-      minutes = '0' + minutes;
+   const times = data.features[0].properties.coordTimes.map(x => {
+      const date = new Date(new Date(x).toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }));
+      const hours = date.getHours() - 12;
+      let minutes = date.getMinutes();
+      if (minutes < 10) {
+         minutes = '0' + minutes;
+      }
+      return hours + ':' + minutes + ' PM';
+   })
+
+
+   const drawMap = curr => {
+      group.clearLayers();
+      const filtered = line.slice(0, curr)
+      new L.Polyline(filtered, {color: 'rgba(101,99,226, 100)', weight: 6}).addTo(group);
    }
-   return hours + ':' + minutes + ' PM';
-});
+   const max = data.features[0].geometry.coordinates.length;
 
+   let distance = 0;
+   let z  = 0;
 
-const drawMap = (curr) => {
-   group.clearLayers();
-   const filtered = line.slice(0, curr)
-   new L.Polyline(filtered, {color: 'rgba(101,99,226, 100)', weight: 6}).addTo(group);
-}
-const max = data.features[0].geometry.coordinates.length;
-
-let distance = 0;
-let z  = 0;
-
-document.body.onkeydown = ({code}) => {
-   if (code === 'Space') {
-      const interval = setInterval(() => {
-         if (z === max) {
-            clearInterval(interval)
-         } else {
-            drawMap(z);
-         }
-         if (z !== 0) {
-            distance += turf.distance(
-               [line[z - 1][1], line[z - 1][0]],
-               [line[z][1], line[z][0]],
-               {units: 'miles'}
-            )
-            document.getElementById('distance').innerText = Math.round(distance * 10 ) / 10 +  ' miles';
-         }
-         if (z % 20 === 0) {
-            document.getElementById('time').innerText = times[z];
-            console.log(times[z])
-         }
-         z++;
-      }, 5)
+   document.body.onkeydown = ({code}) => {
+      if (code === 'Space') {
+         const interval = setInterval(() => {
+            if (z === max) {
+               clearInterval(interval)
+            } else {
+               drawMap(z);
+            }
+            if (z !== 0) {
+               distance += turf.distance(
+                  [line[z - 1][1], line[z - 1][0]],
+                  [line[z][1], line[z][0]],
+                  {units: 'miles'}
+               )
+               document.getElementById('distance').innerText = Math.round(distance * 10 ) / 10 +  ' miles';
+            }
+            if (z % 20 === 0) {
+               document.getElementById('time').innerText = times[z];
+            }
+            z++;
+         }, 5)
+      }
    }
-}
+
+})();
+
